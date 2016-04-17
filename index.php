1c8fb025c1bcba729c2dbefc3bbd66aebc7eba66
Darth Vader <xaxa1@sibmail.com>
2016-04-17 19:53:32 +0600

    домашка 2 занятия

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 ?>
 <font size = "50"><center><br><marquee direction = "up", bgcolor = "red", width = "200"></center>
 ХУЙ!!!
-</marquee>
+</marquee></font><br>
 <?php
 
 function ab($a, $b) {
@@ -26,31 +26,132 @@ function ab($a, $b) {
 		echo "$c<br>";
 	}
 }
+
+function rec($x){
+	while ($x <= 15) {
+		echo "$x ";
+		$x = $x + 1;
+	}
+}	
 ab(7, 5);
 ab(4, 5);
 ab(0, 0);
-/*
+$x = 7;
+switch ($x){
+	case 0: rec($x);break;
+	case 1: rec($x);break;
+	case 2: rec($x);break;
+	case 3: rec($x);break;
+	case 4: rec($x);break;
+	case 5: rec($x);break;
+	case 6: rec($x);break;
+	case 7: rec($x);break;
+	case 8: rec($x);break;
+	case 9: rec($x);break;
+	case 10: rec($x);break;
+	case 11: rec($x);break;
+	case 12: rec($x);break;
+	case 13: rec($x);break;
+	case 14: rec($x);break;
+	case 15: rec($x);break;
+}
+
 function sum1($a, $b){
 	return $a + $b;
 }
 function div1($a, $b){
-	if $b <> 0 {
+	if ($b == 0)	{
+	echo "You can't division on zero!";
+	}
+	else
 	return $a / $b;
 	}
-	else echo "You can't division on zero!";
-}
+
 function mult1($a, $b){
 	return $a * $b;
 }
 function sub1($a, $b){
 	return $a - $b;
 }
-//echo sum(-1, 4);
-//echo div(-1, 4);
-//echo mult(-1, 4);
-//echo sub(-1, 4);
+echo "<br>";
+echo sum1(-1, 4);
+echo "<br>";
+echo div1(-1, 4);
+echo "<br>";
+echo mult1(-1, 4);
+echo "<br>";
+echo sub1(-1, 4);
+echo "<br>";
 
-*/
+function abc($a, $b, $op){
+	switch ($op)
+	{
+	case 'sum': echo sum1($a, $b);
+	break;
+	case 'div': echo div1($a, $b);
+	break;
+	case 'mult':echo  mult1($a, $b);
+	break;
+	case 'sub':echo sub1($a, $b);
+	break;
+	}
+}
+abc(1, 5, mult);
+
+function power ($val, $pow) {
+	$res = $val;
+	if ($pow > 0) {
+		while ($pow > 1){
+			$res = $res * $val;
+			$pow = $pow - 1;
+		}
+	return $res;
+	}
+	elseif ($pow < 0) {
+		$pow = abs($pow);
+		while ($pow > 1){
+			$res = $res * $val;
+			$pow = $pow - 1;
+		}
+	return (1 / $res);
+	}
+	else
+	return 1;
+}
+
+echo "<br>";
+echo power(3, 3);
+echo "<br>";
+echo date('G i s');
+echo "<br>";
+
+
+
+function tt () {
+	$ch = date("G");
+	if ($ch == 1 || $ch == 21){
+		$c = "час";
+	}
+	elseif  ($ch == 2 || $ch == 3 || $ch == 4 || $ch == 22 || $ch == 23){
+		 $c = "часа";
+	}
+	else $c = "часов";
+	
+	$mi = date(i);
+	if ($mi == 1 || $mi == 21 || $mi == 31 || $mi == 41 || $mi == 51){
+		 $m = "минута";
+	}
+	elseif ($mi == 2 || $mi == 3 || $mi == 4 || $mi == 22 || $mi == 23 || $mi == 24 || 
+$mi == 32 || $mi == 33 || $mi == 34 || $mi == 42 || $mi == 43 || $mi == 44 || $mi == 52 || 
+$mi == 53 || $mi == 54){
+		 $m = "минуты";
+		}
+	else $m = "минут";
+		
+	echo date("G $c i $m");
+}
+
+tt();
 
 
 ?>
