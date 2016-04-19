ce87e7faee935d27c3bb2262e0e8a7082fbf2f9b
Darth Vader <xaxa1@sibmail.com>
2016-04-19 11:12:39 +0600

    regex - done

--- index.php
+++ index.php
@@ -13,65 +13,65 @@
 <?php
 
 function ab($a, $b) {
-	if ($a >= 0 && $b >= 0){
-		$c = $a - $b;
-		echo "$c<br>";
-	}
-	elseif ($a < 0 && $b < 0){
-		$c = $a * $b;
-		echo "$c<br>";
-	}
-	else {
-		$c = $a + $b;
-		echo "$c<br>";
-	}
+if ($a >= 0 && $b >= 0){
+	$c = $a - $b;
+	echo "$c<br>";
+}
+elseif ($a < 0 && $b < 0){
+	$c = $a * $b;
+	echo "$c<br>";
+}
+else {
+	$c = $a + $b;
+	echo "$c<br>";
+}
 }
 
 function rec($x){
-	while ($x <= 15) {
-		echo "$x ";
-		$x = $x + 1;
-	}
+while ($x <= 15) {
+	echo "$x ";
+	$x = $x + 1;
+}
 }	
 ab(7, 5);
 ab(4, 5);
 ab(0, 0);
 $x = 7;
 switch ($x){
-	case 0: rec($x);break;
-	case 1: rec($x);break;
-	case 2: rec($x);break;
-	case 3: rec($x);break;
-	case 4: rec($x);break;
-	case 5: rec($x);break;
-	case 6: rec($x);break;
-	case 7: rec($x);break;
-	case 8: rec($x);break;
-	case 9: rec($x);break;
-	case 10: rec($x);break;
-	case 11: rec($x);break;
-	case 12: rec($x);break;
-	case 13: rec($x);break;
-	case 14: rec($x);break;
-	case 15: rec($x);break;
+case 0: rec($x);break;
+case 1: rec($x);break;
+case 2: rec($x);break;
+case 3: rec($x);break;
+case 4: rec($x);break;
+case 5: rec($x);break;
+case 6: rec($x);break;
+case 7: rec($x);break;
+case 8: rec($x);break;
+case 9: rec($x);break;
+case 10: rec($x);break;
+case 11: rec($x);break;
+case 12: rec($x);break;
+case 13: rec($x);break;
+case 14: rec($x);break;
+case 15: rec($x);break;
 }
 
 function sum1($a, $b){
-	return $a + $b;
+return $a + $b;
 }
 function div1($a, $b){
-	if ($b == 0)	{
-	echo "You can't division on zero!";
-	}
-	else
-	return $a / $b;
-	}
+if ($b == 0)	{
+echo "You can't division on zero!";
+}
+else
+return $a / $b;
+}
 
 function mult1($a, $b){
-	return $a * $b;
+return $a * $b;
 }
 function sub1($a, $b){
-	return $a - $b;
+return $a - $b;
 }
 echo "<br>";
 echo sum1(-1, 4);
@@ -84,39 +84,39 @@ echo sub1(-1, 4);
 echo "<br>";
 
 function abc($a, $b, $op){
-	switch ($op)
-	{
-	case 'sum': echo sum1($a, $b);
-	break;
-	case 'div': echo div1($a, $b);
-	break;
-	case 'mult':echo  mult1($a, $b);
-	break;
-	case 'sub':echo sub1($a, $b);
-	break;
-	}
+switch ($op)
+{
+case 'sum': echo sum1($a, $b);
+break;
+case 'div': echo div1($a, $b);
+break;
+case 'mult':echo  mult1($a, $b);
+break;
+case 'sub':echo sub1($a, $b);
+break;
+}
 }
 abc(1, 5, mult);
 
 function power ($val, $pow) {
-	$res = $val;
-	if ($pow > 0) {
-		while ($pow > 1){
-			$res = $res * $val;
-			$pow = $pow - 1;
-		}
-	return $res;
+$res = $val;
+if ($pow > 0) {
+	while ($pow > 1){
+		$res = $res * $val;
+		$pow = $pow - 1;
 	}
-	elseif ($pow < 0) {
-		$pow = abs($pow);
-		while ($pow > 1){
-			$res = $res * $val;
-			$pow = $pow - 1;
-		}
-	return (1 / $res);
+return $res;
+}
+elseif ($pow < 0) {
+	$pow = abs($pow);
+	while ($pow > 1){
+		$res = $res * $val;
+		$pow = $pow - 1;
 	}
-	else
-	return 1;
+return (1 / $res);
+}
+else
+return 1;
 }
 
 echo "<br>";
@@ -128,59 +128,74 @@ echo "<br>";
 
 
 function tt () {
-	$ch = date("G");
-	if ($ch == 1 || $ch == 21){
-		$c = "час";
-	}
-	elseif  ($ch == 2 || $ch == 3 || $ch == 4 || $ch == 22 || $ch == 23){
-		 $c = "часа";
-	}
-	else $c = "часов";
-	
-	$mi = date(i);
-	if ($mi == 1 || $mi == 21 || $mi == 31 || $mi == 41 || $mi == 51){
-		 $m = "минута";
-	}
-	elseif ($mi == 2 || $mi == 3 || $mi == 4 || $mi == 22 || $mi == 23 || $mi == 24 || 
+$ch = date("G");
+if ($ch == 1 || $ch == 21){
+	$c = "час";
+}
+elseif ($ch == 2 || 
+	 $ch == 3 ||
+	 $ch == 4 || 
+	 $ch == 22 || 
+	 $ch == 23) {
+	 $c = "часа";
+	 }
+else $c = "часов";
+
+$mi = date(i);
+if ($mi == 1 || $mi == 21 || $mi == 31 || $mi == 41 || $mi == 51){
+	 $m = "минута";
+}
+elseif ($mi == 2 || $mi == 3 || $mi == 4 || $mi == 22 || $mi == 23 || $mi == 24 || 
 $mi == 32 || $mi == 33 || $mi == 34 || $mi == 42 || $mi == 43 || $mi == 44 || $mi == 52 || 
 $mi == 53 || $mi == 54){
-		 $m = "минуты";
-		}
-	else $m = "минут";
-		
-	echo date("G $c i $m");
+	 $m = "минуты";
+	}
+else $m = "минут";
+	
+echo date("G $c i $m");
 }
 
 tt();
 echo "<br>";
-function tt_cool(){
-$pat_1 = '^[02345]?1$';
-$pat_2 = '^[02345]?[234]$';
-	if (preg_match($pat_1, date("G"))){
-		$c = "час";
-	}	
-	elseif (preg_match($pat_2, date("G"))) {
-		$c = "часа";
-	}
-	else $c = "часов";
+function tt_cool (){
+$hour1 = date(G);
+$minut1 = date(i);
+$sec1 = date(s);
+$pat_1 = '/^[02345]?1$/';
+$pat_2 = '/^[023456]?[234]$/';
 
-	if (preg_match($pat_1, date("i"))) {
-		$m = "минута";
-	}
-	if (preg_match($pat_2, date("i"))) {
-		$m = "минуты";
-	}
-	else $m = "минут";
-echo date("G $c i $m");
+if (preg_match($pat_1, $hour1 )) {
+	$c = "час";
+}	
+elseif (preg_match($pat_2, $hour1)) {
+	$c = "часа";
+}
+else $c = "часов";
+
+if (preg_match($pat_1, $minut1)) {
+	$m = "минута";
+}
+elseif (preg_match($pat_2, $minut1)) {
+	$m = "минуты";
+}
+else $m = "минут";
+
+if (preg_match($pat_1, $sec1)){
+	$s = "секунда";
+}
+
+elseif (preg_match($pat_2, $sec1 )) {
+	$s = "секунды";
+}
+else $s = "секунд";
+
+
+echo date("G $c i $m s $s");
+echo "<br>";
+echo " $s";
 }
 tt_cool();
-
-
-
 echo "<br>";
-//echo preg_match("[23]", date("G i");
-
-
 
 ?>
   </body>
