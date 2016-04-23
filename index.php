f78ad0ca148a40b8a78be5db5001454301e4464c
Darth Vader <xaxa1@sibmail.com>
2016-04-23 16:05:58 +0600

    G A

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
         } else { f(); }
     })(document, window, "yandex_metrika_callbacks");
 </script>
-<noscript><div><img src="https://mc.yandex.ru/watch/36955145" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<noscript><div><img src="https://mc.yandex.ru/watch/36955145" style="position:absolute; left:<F4>-9999px;" alt="" /></div></noscript>
 <!-- /Yandex.Metrika counter -->
 <head><title> zagolovok </title></head>
 <meta charset="utf-8">
